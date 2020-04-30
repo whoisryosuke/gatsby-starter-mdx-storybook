1581bf10fa82b3e7ff9f9ee860f74177b9a379cd
Ryosuke <ryosuke.san.hana@gmail.com>
2020-04-30 16:42:06 -0700

    :lipstick: Image card styled

--- src/components/Card/ImageCard.tsx
+++ src/components/Card/ImageCard.tsx
@@ -1,22 +1,37 @@
 import * as React from 'react';
 import styled from 'styled-components'
-import { Box, Flex, Heading, Image } from 'rebass/styled-components'
+import { Box, Flex, Heading, Image, Text } from 'rebass/styled-components'
+import { Play } from 'react-feather'
 import BaseCard from "./BaseCard"
-import Avatar from "../../assets/img/ryosuke-avatar-128.png"
 
-interface IImageCardProps {
-  title: string,
-  subheader: string,
+interface ImageCardProps {
+  title: string
+  subtitle: string
+  date: string
   image: string
 }
 
-const ImageCard: React.FunctionComponent<IImageCardProps> = ({title, subheader, image, ...props}) => {
+const ImageCard: React.FunctionComponent<ImageCardProps> = ({ title, subtitle, date, image, ...props}) => {
   return(
     <BaseCard {...props}>
-      <Box width="100%" height={['15em', '15em', '20em']} variant="card.avatar" sx={{ backgroundImage: `url(${image})` }} />
-      <Flex flexWrap='wrap'>
-        <Heading variant="subheader" mb="4" width={[1, 1, 1/2]}>{title}</Heading>
-        <Heading variant="label" mb="4" width={[1, 1, 1/2]} textAlign={['left','left','right']}>{subheader}</Heading>
+      <Image src={image} alt={`Thumbnail for ${title}`} />
+      <Box p={4}>
+        <Heading variant="label" mb="3">
+          {subtitle}
+        </Heading>
+        <Heading variant="h2" mb="3">
+          {title}
+        </Heading>
+      </Box>
+      <Flex
+        p={4}
+        justifyContent="space-between"
+        sx={{ borderTop: '1px solid', borderColor: 'gray.light' }}
+      >
+        <Text variant="small">{date}</Text>
+        <Box>
+          <Play color="#5B5B5B" />
+        </Box>
       </Flex>
     </BaseCard>
   )
