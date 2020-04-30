82d399165a4b0639c0dab524294a8055de5a73ef
Ryosuke <ryosuke.san.hana@gmail.com>
2020-04-30 16:43:29 -0700

    :rotating_light: All card tests

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -3,44 +3,63 @@ import { Box, Flex } from 'rebass/styled-components'
 import CardGroup from './CardGroup'
 import ArticleCard from './ArticleCard'
 import BasicCard from "./BasicCard"
-import ProjectCard from "./ProjectCard"
+import EventCard from "./EventCard"
 import ImageCard from "./ImageCard"
 
 export default { title: 'Card' };
 
+// Dummy data
 const title = 'Getting started developing Shopify themes'
 const subheader = 'Getting Started'
 const description =
   "Shopify has exploded over the past few years, becoming a near de-facto decision for any small to mid scale e-commerce project. It's become more important than ever to sharpen Shopify skills and get a handle on Liquid."
-const items = Array.from('x'.repeat(3)).map(() => <BasicCard
+const date = "April 20, 2020"
+const imageUrl = "http://i3.ytimg.com/vi/r7X9wbeqEtU/maxresdefault.jpg"
+
+// 3 cards in array for group
+const items = Array.from('x'.repeat(3)).map(() => <ArticleCard
                  title={title}
                  subheader={subheader}
                  description={description}
+                 date={date}
                />)
 
 export const basic = () => (
              <BasicCard
                title={title}
                subheader={subheader}
-               description={description}
              />
 )
 export const group = () => (
-         <CardGroup items={items} columns={3} />
-       )
+         <CardGroup columns={3}>{items}</CardGroup>
+)
+export const designSystem = () => (
+  <BasicCard
+    title={title}
+    subheader={subheader}
+    reverse
+  />
+)
 export const article = () => (
          <ArticleCard
            title={title}
            subheader={subheader}
            description={description}
+           date={date}
            link={'/test'}
          />
        )
-export const project = () => <ProjectCard title="Kushy API Documentation" subheader="UI / UX" href="/kushy-api-documentation" />
-export const image = () => <ImageCard title="Kushy API Documentation" subheader="UI / UX" />
-export const imageGrid = () => <Flex flexWrap="wrap">
-  <ImageCard title="Kushy API Documentation" subheader="UI / UX" width={[1, 1/2]} />
-  <ImageCard title="Kushy API Documentation" subheader="UI / UX" width={[1, 1/2]} />
-  <ImageCard title="Kushy API Documentation" subheader="UI / UX" width={[1, 1/2]} />
-  <ImageCard title="Kushy API Documentation" subheader="UI / UX" width={[1, 1/2]} />
-  </Flex>
+export const event = () => (
+         <EventCard
+           title="Design Systems Conf 2020"
+           subheader="San Francisco, CA"
+           date={date}
+           link={'/test'}
+         />
+       )
+export const image = () => <ImageCard title="Managing Colors in a Design System" subtitle="Marcin Treder" date="Dec 13, 2019" image={imageUrl} />
+export const imageGrid = () => <CardGroup columns={3}>
+  <ImageCard title="Managing Colors in a Design System" subtitle="Marcin Treder" date="Dec 13, 2019" image={imageUrl} />
+  <ImageCard title="Managing Colors in a Design System" subtitle="Marcin Treder" date="Dec 13, 2019" image={imageUrl} />
+  <ImageCard title="Managing Colors in a Design System" subtitle="Marcin Treder" date="Dec 13, 2019" image={imageUrl} />
+</CardGroup>
