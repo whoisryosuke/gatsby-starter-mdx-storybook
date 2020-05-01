1b5ad96e4f2622dbde80278645b100877d244364
Ryosuke <ryosuke.san.hana@gmail.com>
2020-05-01 11:32:23 -0700

    :bug: Hover state for white btn

--- src/components/Masthead/Masthead.tsx
+++ src/components/Masthead/Masthead.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
 import { Box, Button, Flex, Heading } from 'rebass/styled-components'
 
-interface IMastheadProps {
+interface MastheadProps {
   header: string
-  subheader: string
+  subtitle: string
 }
 
-const Masthead: React.FunctionComponent<IMastheadProps> = ({header, subheader}) => {
+const Masthead: React.FunctionComponent<MastheadProps> = ({header, subtitle}) => {
   return (
     <Box as="section" px={4} py={6} textAlign="center">
       <Heading fontSize={[5, 6, 7]} mb={3}>{header}</Heading>
-      <Heading fontSize={[1, 2, 3]} fontWeight="normal" mb={4}>{subheader}</Heading>
+      <Heading fontSize={[1, 2, 3]} fontWeight="normal" mb={4}>{subtitle}</Heading>
       <Flex justifyContent="center" mb={3}>
         <Button variant="masthead" mr={3}>Browse systems</Button>
-        <Button variant="masthead" color="black" bg="white">Start learning</Button>
+        <Button variant="masthead" color="black" bg="white" sx={{
+          ':hover,:focus,.active': {
+            color: 'black',
+            bg: 'white',
+          },}}>Start learning</Button>
       </Flex>
     </Box>
   )
