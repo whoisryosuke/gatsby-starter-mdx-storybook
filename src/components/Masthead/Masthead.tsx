cb8d53f375dfae2bf3ff905556e3f193578c3a32
Ryosuke <ryosuke.san.hana@gmail.com>
2020-04-30 18:21:08 -0700

    :zap: Added hardcoded buttons

--- src/components/Masthead/Masthead.tsx
+++ src/components/Masthead/Masthead.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box, Heading } from 'rebass/styled-components'
+import { Box, Button, Flex, Heading } from 'rebass/styled-components'
 
 interface IMastheadProps {
   header: string
@@ -8,9 +8,13 @@ interface IMastheadProps {
 
 const Masthead: React.FunctionComponent<IMastheadProps> = ({header, subheader}) => {
   return (
-    <Box as="section" p={4} textAlign="center">
-      <Heading fontSize={[5, 6, 7]}>{header}</Heading>
-      <Heading fontSize={[1, 2, 3]}>{subheader}</Heading>
+    <Box as="section" px={4} py={6} textAlign="center">
+      <Heading fontSize={[5, 6, 7]} mb={3}>{header}</Heading>
+      <Heading fontSize={[1, 2, 3]} fontWeight="normal" mb={4}>{subheader}</Heading>
+      <Flex justifyContent="center" mb={3}>
+        <Button variant="masthead" mr={3}>Browse systems</Button>
+        <Button variant="masthead" color="black" bg="white">Start learning</Button>
+      </Flex>
     </Box>
   )
 };
