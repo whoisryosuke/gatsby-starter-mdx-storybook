f3d0325ac41dd290331b466773d9048c138f8798
Ryosuke <ryosuke.san.hana@gmail.com>
2020-04-30 17:28:08 -0700

    :lipstick: Link styled

--- src/components/Link/Link.stories.tsx
+++ src/components/Link/Link.stories.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import Link from "./Link"
-import {Button} from 'rebass/styled-components'
+import { Button, Heading, Text } from 'rebass/styled-components'
 import ButtonOutline from "../Button/ButtonOutline"
 
 export default { title: 'Link' };
 
 export const basic = () => <Link to="/blog">Go to blog</Link>
-export const styledText = () => <Link to="/blog"><Text variant="label">Go to blog</Text></Link>
-export const button = () => <Link to="/blog"><Button variant="label">Go to blog</Button></Link>
-export const buttonOutline = () => <Link to="/blog"><ButtonOutline>Go to blog</ButtonOutline></Link>
+export const secondary = () => <Link to="/blog" secondary>Go to blog</Link>
+export const styledText = () => <Link to="/blog" secondary><Text variant="label">Go to blog</Text></Link>
--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,24 +1,29 @@
 import * as React from 'react'
 import GLink from 'gatsby-link'
 import styled from 'styled-components'
-import {Box} from 'rebass/styled-components'
 
 const StyledLink = styled(GLink)`
-  border-bottom:0;
-  ${props => props.border && `
-    border-bottom:1px;
+  font-family: ${({theme}) => theme.fonts.body};
+  color:${({theme}) => theme.colors.primary};
+  font-weight:bold;
+  border-bottom:1px;
+  text-decoration:none;
+
+  ${props => props.secondary && `
+    color:${props.theme.colors.black};
   `}
 `
 
 interface Props {
-  children: React.Node[],
-  to: string,
+  children: React.JSXElement
+  to: string
+  secondary?: boolean
 }
 
-export const Link: React.FC<Props> = ({children, to}) => {
+export const Link: React.FC<Props> = ({ children, secondary, to}) => {
   
   return (
-    <StyledLink to={to}>
+    <StyledLink to={to} secondary>
       {children}
     </StyledLink>
   )
